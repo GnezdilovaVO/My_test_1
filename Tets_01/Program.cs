8e6c488adc2e004ef5a93de927270baa5a3f5dfd
Valentina <valyapomortseva@gmail.com>
2023-04-06 18:05:59 +0700

    Решила задачу в лоб. Исправила диаграмму

--- Tets_01/Program.cs
+++ Tets_01/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// using System.Linq;
+// using System.Text;
+String [] array1 = { "Hello","2", "world", ":-)" };
+String [] array2 = new String[array1.Length];
+int count = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if (array1[i].Length <=3)
+    {
+        count++;
+        array2[count] = array1[i];
+        Console.Write(array2[count] + " ");
+    }
+}
